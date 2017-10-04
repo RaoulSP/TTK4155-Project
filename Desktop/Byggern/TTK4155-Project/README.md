48d4b568fb4363054d14dfdf28b1356f145a0352
unknown <peterhs@ITK-D659.win.ntnu.no>
2017-10-04 11:20:52 +0200

    removing trash

--- Desktop/Byggern/TTK4155-Project/README.md
+++
@@ -1,2 +0,0 @@
-# TTK4155-Project
-Term Project for TTK4155 "Byggern" at NTNU
--- Desktop/Byggern/TTK4155-Project/TTK4155-Project/TTK4155 Gruppe 44/TTK4155 Gruppe 44/touchModule.c
+++
@@ -1,8 +0,0 @@
-#include <avr/io.h>
-#include "utils.h"
-
-
-void touchModule_init(){
-	clear_bit(DDRB, PB0); //Set DDR to input for button
-	clear_bit(DDRB, PB1); //Set DDR to input for button
-}
